b2c126a9216d920288f9a3d3e3368df9fbbf7213
chinmay5525 <chinmayawade32@yahoo.com>
2025-01-04 20:09:09 +0530

    554

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,4 @@
 import './Home.css'
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import ImageOne from '../assets/image.png'
 import SectorOne from '../assets/sectors/SectorOne.jpg'
 import SectorTwo from '../assets/sectors/SectorTwo.jpg'
@@ -228,29 +226,9 @@ const Home = () => {
     }, []);
 
 
-    const [isMobile, setIsMobile] = useState(false);
 
-    useEffect(() => {
-        const handleResize = () => {
-            setIsMobile(window.innerWidth <= 1024); // Adjusted breakpoint for tablets and smaller devices
-        };
 
-        // Check screen size on load and resize
-        handleResize();
-        window.addEventListener("resize", handleResize);
 
-        // Cleanup listener on unmount
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
-
-    useEffect(() => {
-        if (isMobile) {
-            // Enable automatic carousel sliding
-            const carousel = document.querySelector("#workCarousel");
-            if (carousel) {
-            }
-        }
-    }, [isMobile]);
     return (
         <div>
             <section className="landing-section">
@@ -557,104 +535,48 @@ const Home = () => {
                     </div>
                 </div>
             </section>
-            <section className="location-section">
-                <div>
-                    <h1 className="locations">
-                        Culture-Driven UI/UX <br />
-                        & Development Excellence
-                    </h1>
-                    <p className="location-subheading">
-                        We're a group of 180+ creative minds spread <br />
-                        across 4 countries touching billions of lives <br />
-                        through design.
-                    </p>
-                    <div className="countries-container">
-                        <div className="countries-grid">
-                            <div>India <svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 -960 960 960" width="26px" fill="#5f6368"><path d="M215.39-180 180-215.39l474.36-474.35H361.9V-740H740v378.1h-50.26v-292.46L215.39-180Z" /></svg></div>
-                            <div>Great Britain <svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 -960 960 960" width="26px" fill="#5f6368"><path d="M215.39-180 180-215.39l474.36-474.35H361.9V-740H740v378.1h-50.26v-292.46L215.39-180Z" /></svg></div>
-                            <div>UAE <svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 -960 960 960" width="26px" fill="#5f6368"><path d="M215.39-180 180-215.39l474.36-474.35H361.9V-740H740v378.1h-50.26v-292.46L215.39-180Z" /></svg></div>
-                            <div>Singapore <svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 -960 960 960" width="26px" fill="#5f6368"><path d="M215.39-180 180-215.39l474.36-474.35H361.9V-740H740v378.1h-50.26v-292.46L215.39-180Z" /></svg></div>
-                            <div>Russia <svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 -960 960 960" width="26px" fill="#5f6368"><path d="M215.39-180 180-215.39l474.36-474.35H361.9V-740H740v378.1h-50.26v-292.46L215.39-180Z" /></svg></div>
-                            <div>South Africa <svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 -960 960 960" width="26px" fill="#5f6368"><path d="M215.39-180 180-215.39l474.36-474.35H361.9V-740H740v378.1h-50.26v-292.46L215.39-180Z" /></svg></div>
-                            <div>Kazakhstan <svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 -960 960 960" width="26px" fill="#5f6368"><path d="M215.39-180 180-215.39l474.36-474.35H361.9V-740H740v378.1h-50.26v-292.46L215.39-180Z" /></svg></div>
-                        </div>
-                    </div>
-                    <div className="pt-5 pb-3">
-                        {isMobile ? (
-                            // Carousel for Mobile
-                            <div
-                                id="workCarousel"
-                                className="carousel slide"
-                                data-bs-ride="carousel"
-                            >
-                                <div className="carousel-inner">
-                                    <div className="carousel-item active">
-                                        <img
-                                            src={WorkOne}
-                                            alt="Work One"
-                                            className="d-block w-100 work-image"
-                                        />
-                                    </div>
-                                    <div className="carousel-item">
-                                        <img
-                                            src={WorkTwo}
-                                            alt="Work Two"
-                                            className="d-block w-100 work-image"
-                                        />
-                                    </div>
-                                    <div className="carousel-item">
-                                        <img
-                                            src={WorkThree}
-                                            alt="Work Three"
-                                            className="d-block w-100 work-image"
-                                        />
-                                    </div>
-                                </div>
 
-                                {/* Hide arrows */}
-                                <style>
-                                    {`
-                  #workCarousel .carousel-control-prev,
-                  #workCarousel .carousel-control-next {
-                    display: none;
-                  }
-                `}
-                                </style>
+
+            <section className='location-section'>
+                <div>
+                    <div>
+                        <h1 className="locations">
+                            Culture-Driven UI/UX <br />
+                            & Development Excellence
+                        </h1>
+                        <p className='location-subheading'>We're a group of 180+ creative minds spread <br /> across 4 countries touching billions of lives <br />through design.</p>
+                        <div className="countries-container">
+                            <div className="countries-grid">
+                                <div>India <svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 -960 960 960" width="26px" fill="#5f6368"><path d="M215.39-180 180-215.39l474.36-474.35H361.9V-740H740v378.1h-50.26v-292.46L215.39-180Z" /></svg></div>
+                                <div>United Kingdom <svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 -960 960 960" width="26px" fill="#5f6368"><path d="M215.39-180 180-215.39l474.36-474.35H361.9V-740H740v378.1h-50.26v-292.46L215.39-180Z" /></svg></div>
+                                <div>UAE <svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 -960 960 960" width="26px" fill="#5f6368"><path d="M215.39-180 180-215.39l474.36-474.35H361.9V-740H740v378.1h-50.26v-292.46L215.39-180Z" /></svg></div>
+                                <div>Singapore <svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 -960 960 960" width="26px" fill="#5f6368"><path d="M215.39-180 180-215.39l474.36-474.35H361.9V-740H740v378.1h-50.26v-292.46L215.39-180Z" /></svg></div>
+                                <div>Russia <svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 -960 960 960" width="26px" fill="#5f6368"><path d="M215.39-180 180-215.39l474.36-474.35H361.9V-740H740v378.1h-50.26v-292.46L215.39-180Z" /></svg></div>
+                                <div>South Africa <svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 -960 960 960" width="26px" fill="#5f6368"><path d="M215.39-180 180-215.39l474.36-474.35H361.9V-740H740v378.1h-50.26v-292.46L215.39-180Z" /></svg></div>
+                                <div>Kazakhstan <svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 -960 960 960" width="26px" fill="#5f6368"><path d="M215.39-180 180-215.39l474.36-474.35H361.9V-740H740v378.1h-50.26v-292.46L215.39-180Z" /></svg></div>
                             </div>
-                        ) : (
-                            // Grid for Desktop/Web
+                        </div>
+                        <div className='pt-5 pb-3'>
                             <div className="container-fluid">
                                 <div className="row">
                                     <div className="col-sm">
-                                        <img
-                                            src={WorkOne}
-                                            alt="Work One"
-                                            loading="lazy"
-                                            className="work-image"
-                                        />
+                                        <img src={WorkOne} alt="fireSpot" loading="lazy" className="work-image" />
                                     </div>
                                     <div className="col-sm">
-                                        <img
-                                            src={WorkTwo}
-                                            alt="Work Two"
-                                            loading="lazy"
-                                            className="work-image"
-                                        />
+                                        <img src={WorkTwo} alt="fireSpot" loading="lazy" className="work-image" />
                                     </div>
                                     <div className="col-sm">
-                                        <img
-                                            src={WorkThree}
-                                            alt="Work Three"
-                                            loading="lazy"
-                                            className="work-image"
-                                        />
+                                        <img src={WorkThree} alt="fireSpot" loading="lazy" className="work-image" />
                                     </div>
                                 </div>
                             </div>
-                        )}
+                        </div>
                     </div>
                 </div>
             </section>
+
+
+
             <section>
                 <div className="container my-5">
                     <div className="row">
